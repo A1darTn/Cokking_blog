0ac64c78b06b73ce3fd7eb9339a1dc57c806337a
Aidar <aidar_tn@icloud.com>
2025-05-01 22:35:25 +0300

    Добавление зависимостей, настройка бд под Postgressql

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "conf.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Name your database",
+        "HOST": "Your host",
+        "PORT": "Your port",
+        "PASSWORD": "Your password database",
+        "USER": "User database",
     }
 }
 
